42c72610cee880b1d28cded8a17a680fe2febf75
sc4599 <sc4599@163.com>
2015-09-11 16:25:25 +0800

    提交练习题 27页 10道题+答案

--- base/src/HomeWork27.scala
+++ base/src/HomeWork27.scala
@@ -69,15 +69,20 @@ object HomeWork27 {
     if(s.length==1)return s.take(1).toLong
     else s.take(1).charAt(0).toLong*product3(s.drop(1))
   }
-  //10
-//  var x=3
-//  def mee(n:Int,var x): Int ={
-//    val y=if(n%2==0&&n>0){
-//      mee(n+2)
-//    }
-//    x
-//
-//  }
+  //10 编写函数计算xn,其中n是整数，使用如下的递归定义:
+  //  xn=y2,如果n是正偶数的话，这里的y=x(n/2)
+  //  xn = x*x(n-1),如果n是正奇数的话
+  //  x0 = 1
+  //   xn = 1/x(-n),如果n是负数的话
+ // 不得使用return语句
+  def mi(x:Int,n:Int): Double ={
+    if(n==0)1
+    else if(n>0&& n%2==0) mi(x,n/2)*mi(x,n/2)
+    else if(n>0&& n%2==1) x*mi(x,n-1)
+    else 1/mi(x,-n)
+  }
+
+
   def main(args: Array[String]): Unit = {
 //    product3("Hello")
 //    val s:String="Hello"
